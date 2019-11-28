9e61693bdd6062a13491f89a26d2b8063a24a362
EC2 Default User <ec2-user@ip-172-31-39-81.ap-northeast-1.compute.internal>
2019-11-28 14:27:19 +0000

    attendance-A-users.index.erb_アコーディオン機能なしの_編集保存できるform_with版

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :edit_basic_info, :update_basic_info, :destroy]
   before_action :logged_in_user, only: [:index, :show, :edit, :update, :edit_basic_info, :update_basic_info, :destroy]
-  before_action :correct_user, only: [:edit, :update]
-  before_action :admin_user, only: [:index, :edit_basic_info, :update_basic_info, :destroy]
+  #before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: [:index, :edit, :update, :edit_basic_info, :update_basic_info, :destroy]
   before_action :admin_or_correct_user, only: :show
   before_action :set_one_month, only: :show
 
@@ -69,7 +69,9 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :affiliation, :employee_number, :uid,
+                                    :basic_work_time, :designated_work_start_time, :designated_work_end_time,
+                                    :password, :password_confirmation)
     end
 
     def basic_info_params
