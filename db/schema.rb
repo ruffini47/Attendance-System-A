4a132005e0935ea5d98dc34c99d1c4fdd5f8141b
EC2 Default User <ec2-user@ip-172-31-39-81.ap-northeast-1.compute.internal>
2019-12-23 07:55:34 +0000

    attendance-A-重複残業申請失敗

--- db/schema.rb
+++ db/schema.rb
@@ -57,9 +57,9 @@ ActiveRecord::Schema.define(version: 20191222014217) do
     t.integer "employee_number"
     t.string "uid"
     t.string "affiliation"
-    t.datetime "basic_work_time", default: "2019-12-21 23:00:00"
-    t.datetime "designated_work_start_time", default: "2019-12-22 00:00:00"
-    t.datetime "designated_work_end_time", default: "2019-12-22 09:00:00"
+    t.datetime "basic_work_time", default: "2019-12-22 23:00:00"
+    t.datetime "designated_work_start_time", default: "2019-12-23 00:00:00"
+    t.datetime "designated_work_end_time", default: "2019-12-23 09:00:00"
     t.integer "number_of_overtime_applied", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
   end
