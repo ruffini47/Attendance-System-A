042a01c92d5fec4ce73f8566d43f411b923d195c
EC2 Default User <ec2-user@ip-172-31-39-81.ap-northeast-1.compute.internal>
2020-01-02 14:55:40 +0000

    attendance-A-出勤時間＞退勤時間の場合のみバリデーションを設定する

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -11,8 +11,9 @@ class Attendance < ApplicationRecord
   
   # 出勤時間が存在しない場合、退勤時間は無効
   validate :finished_at_is_invalid_without_a_started_at
+
   # 出勤・退勤時間どちらも存在するとき、出勤時間よりも早い退勤時間は無効
-  #validate :started_at_than_finished_at_fast_if_invalid
+  validate :temp_after_change_start_time_than_temp_after_change_end_time_fast_if_invalid
   
   def started_at_is_invalid_without_a_finished_at
     errors.add(:finished_at, "が必要です") if started_at.present? && finished_at.blank?
@@ -22,9 +23,9 @@ class Attendance < ApplicationRecord
     errors.add(:started_at, "が必要です") if started_at.blank? && finished_at.present?
   end
   
-  #def started_at_than_finished_at_fast_if_invalid
-  #  if started_at.present? && finished_at.present?
-  #    errors.add(:started_at, "より早い退勤時間は無効です") if started_at > finished_at
-  #  end
-  #end
+  def temp_after_change_start_time_than_temp_after_change_end_time_fast_if_invalid
+    if temp_after_change_start_time.present? && temp_after_change_end_time.present?
+      errors.add(:temp_after_change_start_time, "より早い退勤時間は無効です") if temp_after_change_start_time > temp_after_change_end_time
+    end
+  end
 end
