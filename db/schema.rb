9cb4d1cf5771a9b30639a4266414642795681547
EC2 Default User <ec2-user@ip-172-31-39-81.ap-northeast-1.compute.internal>
2020-01-31 12:21:10 +0000

    attendance-A-config/environment.rbファイル修正_db/migrate/20191228122503_remove_number_of_attendance_change_applied_from_attendances.rb修正

--- db/schema.rb
+++ db/schema.rb
@@ -91,11 +91,10 @@ ActiveRecord::Schema.define(version: 20200129135819) do
     t.integer "employee_number"
     t.string "uid"
     t.string "affiliation"
-    t.datetime "basic_work_time", default: "2020-01-28 23:00:00"
-    t.datetime "designated_work_start_time", default: "2020-01-29 00:00:00"
-    t.datetime "designated_work_end_time", default: "2020-01-29 09:00:00"
+    t.datetime "basic_work_time", default: "2020-01-30 23:00:00"
+    t.datetime "designated_work_start_time", default: "2020-01-31 00:00:00"
+    t.datetime "designated_work_end_time", default: "2020-01-31 09:00:00"
     t.integer "number_of_overtime_applied", default: 0
-    t.integer "number_of_attendance_change_applied", default: 0
     t.integer "number_of_manager_approval_applied", default: 0
     t.integer "time_log_year"
     t.integer "time_log_month"
