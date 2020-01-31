50f48c48d9e3df4aec19be65f1e5993f809c1006
EC2 Default User <ec2-user@ip-172-31-39-81.ap-northeast-1.compute.internal>
2020-01-31 12:55:53 +0000

    attendance-A-number_of_attendance_change_appliedカラムをusersに追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200129135819) do
+ActiveRecord::Schema.define(version: 20200131124933) do
 
   create_table "attendances", force: :cascade do |t|
     t.date "worked_on"
@@ -100,6 +100,7 @@ ActiveRecord::Schema.define(version: 20200129135819) do
     t.integer "time_log_month"
     t.integer "temp_year"
     t.integer "temp_month"
+    t.integer "number_of_attendance_change_applied", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
